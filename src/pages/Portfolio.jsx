c75c29d72c3fb58f8687f38fd6b90b63e17ba51c
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-06 22:13:15 -0600

    added resume page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,4 @@
-import './Portfolio.css'; // Import the CSS file
+import "./Portfolio.css"; // Import the CSS file
 
 export default function Portfolio() {
   return (
@@ -12,9 +12,73 @@ export default function Portfolio() {
             alt="Picture of Brewery Locator application"
           />
           <div className="description">
-            Brew-Loc is a Location-base brewery finder. Enter a city and state
-            and it will return a map of all know breweries in the area with a
-            list with their individual info!
+            Brew-Loc is a location-based brewery finder. Enter a city and state
+            and it will return a map of all known breweries in the area with a
+            list containing their individual info!
+          </div>
+        </a>
+      </div>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://note-taker-35-e6604a508493.herokuapp.com/notes">
+          <img
+            src="./src/assets/notetakerpreview.png"
+            className="img-fluid image"
+            alt="Picture of Note-Taker application"
+          />
+          <div className="description">
+            Note-Taker is a simple noting taking browser app. It saves data to
+            an read/write file.
+          </div>
+        </a>
+      </div>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://vast-meadow-71566-a5568f84ff53.herokuapp.com/">
+          <img
+            src="./src/assets/jatepreview.png"
+            className="img-fluid image"
+            alt="Picture of J.A.T.E application"
+          />
+          <div className="description">
+            J.A.T.E is just another text editor. It is a progressive web
+            application so you can install it on your browser!
+          </div>
+        </a>
+      </div>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://broden123.github.io/passwordgenerator/">
+          <img
+            src="./src/assets/passwordgeneratorpreview.png"
+            className="img-fluid image"
+            alt="Picture of password generator application"
+          />
+          <div className="description">
+            This is a randomized password generator. You can customize the
+            length and which characters you want to use using browser prompts.
+          </div>
+        </a>
+      </div>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://broden123.github.io/dailyplanner/">
+          <img
+            src="./src/assets/dailyplannerpreview.png"
+            className="img-fluid image"
+            alt="Picture of Work Day Scheduler application"
+          />
+          <div className="description">
+            This is a work day planner. It saves data to local storage.
+          </div>
+        </a>
+      </div>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://broden123.github.io/portfolio/">
+          <img
+            src="./src/assets/originalportfoliopreview.png"
+            className="img-fluid image"
+            alt="Picture of my original portfolio"
+          />
+          <div className="description">
+            This is my first portfolio to showcase my progress from the
+            beginning of my full-stack course! It is coded in HTML/CSS.
           </div>
         </a>
       </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,54 @@
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+// Configure the PDF.js worker
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 export default function Resume() {
-    return (
-      <div>
-        <h1>Brayden Smith-Zimmerman</h1>
-        <p>Adding info here later</p>
-      </div>
-    );
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
   }
+  function downloadDocument() {
+    const link = document.createElement("a");
+    link.href = "./src/assets/resume.pdf";
+    link.download = "./src/assets/resume.pdf";
+    link.click();
+  }
+
+  return (
+    <div>
+      <div className="d-flex justify-content-center align-items-center">
+        <Document
+          file="./src/assets/resume.pdf"
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+      <div className="d-flex justify-content-center align-items-center">
+        <button
+          onClick={() => setPageNumber(pageNumber - 1)}
+          disabled={pageNumber <= 1}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setPageNumber(pageNumber + 1)}
+          disabled={pageNumber >= numPages}
+        >
+          Next
+        </button>
+        <button onClick={downloadDocument}>Download</button>
+      </div>
+    </div>
+  );
+}
