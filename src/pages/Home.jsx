230f92e8c59f6432ab508e1df5cd54f1313f901e
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-06 21:08:44 -0600

    added about me, working on portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
     <div className="text-center">
+      <img 
+className="border border-dark shadow-lg img-fluid m-3 " 
+src='./src/assets/profilepic.jpg'  
+alt="Brayden Smith-Zimmerman"
+width="400"/>
       <h1>About Me</h1>
-      <p>Adding info here later</p>
+      <p>I am 24 years old, currently residing in the Austin, Texas area!
+        I attended the University of Idaho, where I earned a bachelors of science in biochemistry. <br />
+        After spending a few years in a labratory setting, I realized that my passion for coding was outweighing my desire to continue in the biomedical field. <br />
+        So, I enrolled in a full-stack devlopment bootcamp in hopes of beginning a career in app development!
+      </p>
     </div>
   );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,23 @@
+import './Portfolio.css'; // Import the CSS file
+
 export default function Portfolio() {
   return (
-    <div>
-      <h1>Brayden Smith-Zimmerman</h1>
-      <p>Adding info here later</p>
+    <div className="d-flex flex-column mb-3 text-center">
+      <h1 className="p-2">Some of My Recent Projects:</h1>
+      <div className="flex-fill p-2 image-container">
+        <a href="https://broden123.github.io/Brewer-Loc/">
+          <img
+            src="./src/assets/brewlocpreview.png"
+            className="img-fluid image"
+            alt="Picture of Brewery Locator application"
+          />
+          <div className="description">
+            Brew-Loc is a Location-base brewery finder. Enter a city and state
+            and it will return a map of all know breweries in the area with a
+            list with their individual info!
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
