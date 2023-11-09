f33710c9338326466617c358a34dd2432134874c
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-09 11:40:59 -0600

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 function Footer() {
-  return <div></div>;
+  return (
+
+  <footer className="d-flex flex-row justify-content-center">
+      <a href="https://www.linkedin.com/in/brayden-smithzimmerman/"><img src="../src/assets/linkedinicon.png" alt="LinkedIn" className="w-25 h-auto"/></a>
+      <a href="https://github.com/broden123"><img src="../src/assets/githubicon.png" alt="Github" className=""/></a>
+  </footer>
+  )
 }
 
 export default Footer;
