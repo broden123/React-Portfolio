5bd6de16c3a8116a4e0c8ec0422892758870c79f
Brayden Smith <brayden.smithzimmerman@gmail.com>
2023-11-09 11:21:53 -0600

    contact page setup

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,30 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+// adding functionality to this later
 export default function Contact() {
   return (
-    <div>
-      <h1>Brayden Smith-Zimmerman</h1>
-      <p>Adding info here later</p>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicInput">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="input" placeholder="Enter Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We will never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicInput">
+        <Form.Label>Message</Form.Label>
+        <Form.Control type="textarea" placeholder="Enter Message" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
